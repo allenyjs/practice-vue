02311c2b239f8c1cc7b3916a6e731064c1348ece
allenyjs <allenyjs@gmail.com>
2018-08-12 13:06:25 +0800

    add list

---
+++ scripts/index.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+    el:"#container",
+    data:{
+        subjects:[
+            {subjectName:"v-for練習", linkUrl:"p1.html"},
+            {subjectName:"v-if練習", linkUrl:"p2.html"},
+            {subjectName:"v-on事件練習", linkUrl:"p3.html"},
+            {subjectName:"Event Modifiers 修飾符練習", linkUrl:"p4.html"},
+            {subjectName:"動態加入Or移除Class練習", linkUrl:"p5.html"},
+            {subjectName:"computed(計算屬性)練習", linkUrl:"p6.html"},
+            {subjectName:"基礎的Two Way Binding練習", linkUrl:"p7.html"},
+        ]
+    }
+});
